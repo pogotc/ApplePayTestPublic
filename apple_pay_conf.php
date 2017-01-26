9ce6ab68f459cdc8427c30f2f8404b0aa7303d7d
Steven Ellis <steve.ellis@mademedia.co.uk>
2017-01-26 11:37:20 +0000

    Adding key files

--- apple_pay_conf.php
+++ apple_pay_conf.php
@@ -1,10 +1,10 @@
 <?php
 // update these with the real location of your two .pem files. keep them above/outside your webroot folder
-define('PRODUCTION_CERTIFICATE_KEY', '/your/path/to/applepay_includes/ApplePay.key.pem');
-define('PRODUCTION_CERTIFICATE_PATH', '/your/path/to/applepay_includes/ApplePay.crt.pem');
+define('PRODUCTION_CERTIFICATE_KEY', __DIR__ . 'ApplePay.key.pem');
+define('PRODUCTION_CERTIFICATE_PATH', __DIR__ . 'ApplePay.crt.pem');
 
 // This is the password you were asked to create in terminal when you extracted ApplePay.key.pem
-define('PRODUCTION_CERTIFICATE_KEY_PASS', 'your password here'); 
+define('PRODUCTION_CERTIFICATE_KEY_PASS', 'password'); 
 
 define('PRODUCTION_MERCHANTIDENTIFIER', openssl_x509_parse( file_get_contents( PRODUCTION_CERTIFICATE_PATH ))['subject']['UID'] ); //if you have a recent version of PHP, you can leave this line as-is. http://uk.php.net/openssl_x509_parse will parse your certificate and retrieve the relevant line of text from it e.g. merchant.com.name, merchant.com.mydomain or merchant.com.mydomain.shop
 // if the above line isn't working for you for some reason, comment it out and uncomment the next line instead, entering in your merchant identifier you created in your apple developer account
